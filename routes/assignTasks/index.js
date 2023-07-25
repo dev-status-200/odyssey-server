c096fe71f3c0da0d08289a854e44173932388eb8
dev-status-200 <dev.status.200@gmail.com>
2023-07-25 02:56:57 -0700

    update

--- routes/assignTasks/index.js
+++ routes/assignTasks/index.js
@@ -1,15 +1,36 @@
+const { Employees } = require("../../functions/Associations/employeeAssociations");
 const { AssignTask } = require("../../models");
 const routes = require('express').Router();
 const Sequelize = require('sequelize');
 
-routes.post('/allTasks', async (req, res) =>{
+routes.post('/createTask', async (req, res) =>{
 try {
     const result = await AssignTask.create(req.body)
     res.json({result: result})
-} catch (error) {
-    
-}
+} 
+catch (error) {
+    res.json({status:'error', result:error});
+  }
 })
+
+routes.get('/getEmployeeTask', async (req, res) =>{
+    try {
+          const result = await AssignTask.findAll({where : {EmployeeId : req.headers.id}, 
+            include: [{
+              model: Employees,
+              as: 'assignedBy',
+              attributes: ['name']
+            }]
+          })
+          res.json({result: result})
+    } 
+    catch (error) {
+        res.json({status:'error', result:error});
+      }
+    })
+    
+    
+
 
 
 module.exports = routes
